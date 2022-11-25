c123cf14a5077d5be2cf66cc901372d6aaa5ba5c
marc0cl <63360454+marc0cl@users.noreply.github.com>
2022-11-25 01:14:32 -0300

    Added retiro mailing flow

---
+++ controllers/retiroController.js
@@ -0,0 +1,29 @@
+var nodemailer = require('nodemailer');
+
+var correo = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'marco.acuna1901@alumnos.ubiobio.cl',
+    pass: 'CAMBIAR'
+  }
+});
+
+const notificarRetiro = (req, res) => {
+    const {rut, parvulo} = req.params;
+    const {to, subject, rutParvulo} = req.body;
+
+    var mailOptions = {
+        from: 'marco.acuna1901@alumnos.ubiobio.cl',
+        to,
+        subject: `Su pupilo ha sido retirado`,
+        text: `Su pupilo: ${rutParvulo} fue retirado a las ${(new Date.now).toLocaleDateString()}`
+      };
+
+      correo.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+}
