3924af120a2b24aecf2dee3f0f6cb2c37a12c734
AlexFT257 <ftb2570@gmail.com>
2022-11-24 23:18:55 -0300

    rutas de archivos en index.js

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
 // insertar las rutas de los requisitos
-const asistenteParvuloRoutes = require('./routes/asistenteParvuloRoutes')
-
+const asistenteParvuloRoutes = require("./routes/asistenteParvuloRoutes");
+const fileRoutes = require("./routes/fileRoutes");
 
 app.use(cors());
 app.use(express.json());
-app.options('*',cors());
+app.options("*", cors());
 
-app.use('/api', asistenteParvuloRoutes);
+//archivos
+//app.use('/api', fileRoutes)
 
-mongoose.set('useNewUrlParser',true);
+app.use("/api", asistenteParvuloRoutes);
+app.use("/api", fileRoutes);
+
+mongoose.set("useNewUrlParser", true);
 //mongoose.set('useFindeAndModify',false); // crashea si se activa debe ser un video que no he visto
-mongoose.set('useCreateIndex',true);
-mongoose.set('useUnifiedTopology',true);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
-mongoose.connect(process.env.DB,(error)=>{
-    if(error){
-        console.log(error);
-    } else {
-        console.log("Connected to database");
-    }
+mongoose.connect(process.env.DB, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connected to database");
+  }
+});
 
-})
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server started on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
