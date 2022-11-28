cd6f0cbffcee87ddb878c0a572794b08b0ab04f8
E. Salazar Rubilar <emerson.salzar1901@alumnos.ubiobio.cl>
2022-11-28 12:33:57 -0300

    fix middlewares

--- routes/actividadRoutes.js
+++ routes/actividadRoutes.js
@@ -5,8 +5,8 @@ const parvulariaAuth = require('../middlewares/parvulariaAuth');
 
 api.post('/actividad', parvulariaAuth, actividadController.createActividad);
 api.get('/actividades',actividadController.getActividades);
-api.put('/actividad/update/:id',actividadController.updateActividad);
-api.delete('/actividad/delete/:id',actividadController.deleteActividad);
-api.get('/actividad/search/:id',actividadController.getActividad);
+api.put('/actividad/update/:id', parvulariaAuth, actividadController.updateActividad);
+api.delete('/actividad/delete/:id', parvulariaAuth, actividadController.deleteActividad);
+api.get('/actividad/search/:id', actividadController.getActividad);
 
 module.exports=api;
