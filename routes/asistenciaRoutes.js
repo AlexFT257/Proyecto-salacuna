2c8dcded495d81ab7654fbc06183a4012184d21c
marc0cl <63360454+marc0cl@users.noreply.github.com>
2022-11-28 01:22:14 -0300

    added auth to post urls

--- routes/asistenciaRoutes.js
+++ routes/asistenciaRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const asistenciaController = require('../controllers/asistenciaController.js');
 const retiroController = require('../controllers/retiroController.js');
 const api = express.Router();
+const parvulariaAuth = require('../middlewares/parvulariaAuth');
 
-api.post('/asistencia/registrar/:id', asistenciaController.registrarAsistencia);
+api.post('/asistencia/registrar/:id',parvulariaAuth,  asistenciaController.registrarAsistencia);
 api.put('/asistencia/actualizar/:id', asistenciaController.actualizarAsistencia);
 api.delete('/asistencia/eliminar/:id', asistenciaController.eliminarAsistencia);
 api.get('/asistencia/obtener', asistenciaController.obtenerAsistencia);
 
-api.post('/asistencia/notificar_retiro', retiroController.notificarRetiro);
+api.post('/asistencia/notificar_retiro',parvulariaAuth, retiroController.notificarRetiro);
 
 module.exports = api;
